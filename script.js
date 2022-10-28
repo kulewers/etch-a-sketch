b2b19770c67afbba98eba26e7ad30090ea7b42f9
kulewers <kule.wers@gmail.com>
2022-10-28 18:44:05 +0300

    Add size button, rework related logic

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const gridBase = document.querySelector('div.gridBase');
+const sizeButton = document.querySelector('button.sizeButton');
+sizeButton.addEventListener('click', function() {
+    let size = 16;
+    do {
+        size = prompt('Select size:');
+    } while (parseInt(size) > 100)
+    gridBase.setAttribute('style', `grid-template-columns: repeat(${size}, 1fr)`);
+    printGrid(size);
+})
 
-for (let i=0; i<256; i++) {
-    const block = document.createElement('div');
-    block.setAttribute('style', 'box-sizing: border-box; border: 1px solid black; width: 30px;');
-    block.addEventListener('mouseover', function(e) {
-        const t = e.target;
-        t.classList.add('blackBackGround');
-    })
-    gridBase.appendChild(block);
+function printGrid(size) {
+    gridBase.innerHTML = '';
+    for (let i=0; i<size ** 2; i++) {
+        const block = document.createElement('div');
+        block.classList.add('gridBlock');
+        block.addEventListener('mouseover', function(e) {
+            const t = e.target;
+            t.classList.add('blackBackGround');
+        })
+        gridBase.appendChild(block);
+    }
 }
